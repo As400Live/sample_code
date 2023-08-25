a1ea353b7ec106eed6bc7b5419073c9a4b6f5d2e
Lance Hampton <lance.hampton@gmail.com>
2023-08-24 23:28:20 -0500

    added stuff

---
+++ functions/main.js
@@ -0,0 +1,26 @@
+// write a function named "add" that adds two numbers
+
+// TODO add function goes here
+var answer = add(1,3);
+console.log(answer);
+
+
+// Write a function named even that returns true
+// if a number is even
+
+
+var result = even(3);
+console.log(result);
+
+result = even(7);
+console.log(result);
+
+// Write a function named mod that returns the modulo
+// of a number
+
+
+
+// Write a function named numbers which takes two numbers
+// and prints the numbers from one to the other
+// for example, numbers(1,10) would print 1 through 10
+
---
+++ scoping/main.js
@@ -0,0 +1,11 @@
+
+var num = 17;
+
+for (var i=0; i<10;i++) {
+    num += 1;
+}
+
+console.log(num);
+
+
+
