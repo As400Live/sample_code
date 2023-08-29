38a4da60a4d58904b54c7f263c12e386458c1826
unknown <liveasrx@gmail.com>
2023-08-28 21:39:32 -0500

    working with functions

--- functions/main.js
+++ functions/main.js
@@ -3,20 +3,19 @@
 // TODO add function goes here
 var answer = add(1,3);
 console.log(answer);
+answer = add (5, answer);
+console.log(answer);
 
 
 // Write a function named even that returns true
 // if a number is even
 
 
-var result = even(3);
+var result = even(4);
 console.log(result);
 
-result = even(7);
-console.log(result);
+// function that prints big if the number is greater than 10
 
-// Write a function named mod that returns the modulo
-// of a number
 
 
 
@@ -24,3 +23,19 @@ console.log(result);
 // and prints the numbers from one to the other
 // for example, numbers(1,10) would print 1 through 10
 
+function add (a,b) {
+    return a + b;
+}
+function even (num) {
+    return num % 2 == 0;
+}
+function big (a){
+    if (a > 10) {
+    console.log("big");
+    } else {
+        console.log("small");
+    }
+}
+big (14);
+big (19);
+big (4);
